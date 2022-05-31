4301c24810b022ecbc61ca4ddddb88fbc1482720
Chizy <chizy7@gmail.com>
2022-05-31 15:23:10 -0400

    Chat VC added

---
+++ Parse Chat/ChatViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChatViewController.h
+//  Parse Chat
+//
+//  Created by Chizaram Chibueze on 5/31/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
